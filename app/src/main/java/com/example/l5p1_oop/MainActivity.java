9ebdd32327c9e1fe708e81215b3d1f32270acb41
ChocolateCharlie <aurore.amrit@gmail.com>
2018-01-06 13:18:56 +0100

    Practice Calling Object Methods (step 18)

--- app/src/main/java/com/example/l5p1_oop/MainActivity.java
+++ app/src/main/java/com/example/l5p1_oop/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.l5p1_oop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import static android.util.TypedValue.COMPLEX_UNIT_SP;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        TextView textView = new TextView((this));
+
+        textView.setText("Hello, World !");
+        textView.setTextColor(0xFFFF0000);
+        textView.setTextSize(COMPLEX_UNIT_SP, 72);
+
+        setContentView(textView);
     }
 }
